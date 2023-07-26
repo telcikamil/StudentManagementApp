26e04f1bcf231c01a834f3af4c94eb6de6512c5f
telcikamil <kamiltelci@outlook.com>
2023-07-26 17:08:19 +0300

    servisler oluşturuldu

---
+++ StudentManagementApp/StudentManagementApp.BLL/Services/Concrete/StudentService.cs
@@ -0,0 +1,20 @@
+﻿using StudentManagementApp.BLL.Services.Abstract;
+using StudentManagementApp.DAL.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementApp.BLL.Services.Concrete
+{
+    public class StudentService:IStudentService
+    {
+        private readonly IStudentRepository studentRepository;
+
+        public StudentService(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+    }
+}
